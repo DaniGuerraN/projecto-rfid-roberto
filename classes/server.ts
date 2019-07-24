ff0933f0fdb959d5189f295fcf9c8f17d55f47d7
unknown <17320@ids.upchiapas.edu.mx>
2019-07-23 23:47:57 -0500

    soy otro cambio

--- classes/server.ts
+++ classes/server.ts
@@ -20,6 +20,7 @@ export default class Server {
         this.app = express();
         this.app.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
             });
